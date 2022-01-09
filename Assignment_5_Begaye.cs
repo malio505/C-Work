27c26bfb8bffc23bc761e93131773768b7585557
Gamaliel B <91226935+malio505@users.noreply.github.com>
2022-01-09 00:52:59 -0700

    Add files via upload

---
+++ Assignment_5_Begaye.cs
@@ -0,0 +1,119 @@
+﻿//Name:Gamaliel Begaye
+//Date:12/16/2021
+//Assignment 5
+
+using System;
+
+namespace Assignment5
+{
+    class assignment5
+    {
+        static void Main(string[] args)
+        {
+            //Declare your variables here 
+            int odd = 0;
+            int even = 0;
+            int input = -1;
+            int max = 0;
+
+
+
+            //Prompt the user for their choice of actions
+            //Console.Write("Enter 1 to display odds, 2 to display evens, or 0 to quit: ");
+            //int input = Convert.ToInt32(Console.ReadLine()); //Program reads user input.
+
+            //Console.Write("Enter the maximum possible value you wish to see: ");
+            //int max = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //Your while loop to execute based on the user's choice of actions
+            while (input != 0)
+            {
+
+                Console.Write("Enter 1 to display odds, 2 to display evens, or 0 to quit: ");
+                input = Convert.ToInt32(Console.ReadLine());
+
+                if (input == 0)
+                {
+                    break;
+                }
+
+                int start = 0;
+              
+                //Your switch statement to output the correct sequence of numbers
+                switch (input)
+                {
+                    //Inside your switch statement, you have two "cases" to consider
+                    //based on the user's choice plus a "default" case that handles 
+                    //when the user does not choose a valid option
+                    //
+                    //Inside each of the two cases, you will need to ask the user for the maximum
+                    //number they wish to see.  You will also need to use an appropriately configured
+                    //for loop (section 6.3 on page 195) to output the numbers as described in the 
+                    //assignment and shown in the example
+
+                    case 0:
+
+                        break;
+
+                    //This should help with your first case statement, you'll need to add the others
+                    case 1:
+                        start = 1;
+
+
+                        //Do the reuquired things here with your for loop
+                        break;
+
+                    //Fill in the remaining cases below, this includes the "default" case
+                    case 2:
+                        start = 0;
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter a value from 0 to 2!: ");
+                        Console.WriteLine();
+                        break; 
+                }
+                //for (int i = 0; i< 4; i++)
+                //{
+
+                //}
+                if (input >= 0 && input <= 2)
+                {
+                    Console.Write("Enter the maximum possible value you wish to see: ");
+                    max = Convert.ToInt32(Console.ReadLine());
+                    int column = 0;
+                    int columnCount = 4;
+
+                    while (start <= max)
+                    {
+                        Console.Write(start + "\t");
+                        if (column == columnCount)
+                        {
+                            Console.Write("\n");
+                            column = 0;
+                        }
+                        column++;
+                        start += 2;
+                    }
+                    Console.WriteLine();
+                }
+              
+                //Re-prompt the user for their choice since this is a sentinel loop
+                
+               
+
+                //int input = Convert.ToInt32(Console.ReadLine()); //Program reads user input. 
+
+            }
+
+            Console.WriteLine("\n\nProgram complete.  Press Enter to continue.");
+            Console.ReadLine();
+
+        } // end Main
+
+    } // end class
+
+} // end namespace
---
+++ Assignment_6_Begaye.cs
@@ -0,0 +1,51 @@
+﻿// Name:Begaye, Gamaliel    
+// Date:1/7/2022
+// Assignment: 6.1
+
+using System;
+
+namespace Assignment6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare two doubles to store the user input 
+            double number1;
+            double number2;
+
+
+            //Get the user input
+            Console.Write("Enter Number 1: ");//User inputs first number.
+            number1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Number 2: ");//User inputs second number. 
+            number2 = Convert.ToDouble(Console.ReadLine());
+
+
+            //Provide the analysis as described in the assignment here
+            //DO NOT CREATE YOUR OWN METHODS!  
+            Console.WriteLine();
+            Console.WriteLine($"The absolute value of {number1} is {Math.Abs(number1)}.");//Absolute value methods.
+
+            Console.WriteLine($"The absolute value of {number2} is { Math.Abs(number2)}.");
+
+            Console.WriteLine($"The maximum of {number1} and {number2} is {Math.Max(number1, number2)}.");//Maximun and Minimum methods.
+
+            Console.WriteLine($"The minimum of {number1} and {number2} is {Math.Min(number1, number2)}.");
+
+            Console.WriteLine($"{number1} raised to the power of 3 is {Math.Pow(number1, 3)}.");//Each number raised to the power of 3 methods.
+
+            Console.WriteLine($"{number2} raised to the power of 3 is {Math.Pow(number2, 3)}.");
+
+            Console.WriteLine($"The square root of the absolute value of {number1} is {Math.Sqrt(Math.Abs(number1))}.");//The square root methods. 
+
+            Console.WriteLine($"The square root of the absolute value of {number2} is {Math.Sqrt(Math.Abs(number2))}.");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Press Enter to continue.");
+            Console.ReadLine();
+
+        }
+    }
+}
